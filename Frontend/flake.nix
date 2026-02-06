0579d18da08001fb107dba6a46283ec792cb4dde
MikkelGaming <70634606+MikkelGaming@users.noreply.github.com>
2026-02-06 12:48:43 +0100

    Added nix flake for nixos development

---
+++ Frontend/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Dotnet dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.dotnet-sdk_8
+        ];
+
+        DOTNET_ROOT = pkgs.dotnet-sdk_8;
+      };
+    };
+}
