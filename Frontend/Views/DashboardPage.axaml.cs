5221bcebdb2955deb0c3f89c002a1f4733ae0871
MikkelGaming <70634606+MikkelGaming@users.noreply.github.com>
2026-02-17 13:55:14 +0100

    Added navbar and switched the old homepage into dashboardpage.

---
+++ Frontend/Views/DashboardPage.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Frontend.Views;
+
+public partial class DashboardPage : UserControl
+{
+    public DashboardPage()
+    {
+        InitializeComponent();
+    }
+
+}
