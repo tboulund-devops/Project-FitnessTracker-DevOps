20a4e9c7159a6f9e8fec2ccb080a0dd91977fa3f
Madsbert <mberthelsen@live.dk>
2026-02-17 13:23:54 +0100

    unit test fix

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -6,30 +6,6 @@ namespace UnitTests;
 
 public class AuthControllerTests : IClassFixture<WebApplicationFactory<Program>>
 {
-    // private readonly WebApplicationFactory<Program> _factory;
 
-    // public AuthControllerTests(WebApplicationFactory<Program> factory)
-    // {
-    //     _factory = factory;
-    // }
-    //
-    // [Fact]
-    // public async Task TrainerLogin_MissingFields_ReturnsBadRequest()
-    // {
-    //     using var client = _factory.CreateClient();
-    //
-    //     var response = await client.PostAsJsonAsync("/api/apiauth/trainer-login", new { Username = "", Password = "" });
-    //
-    //     Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-    // }
-    //
-    // [Fact]
-    // public async Task TrainerLogin_InvalidCredentials_ReturnsUnauthorized()
-    // {
-    //     using var client = _factory.CreateClient();
-    //
-    //     var response = await client.PostAsJsonAsync("/api/apiauth/trainer-login", new { Username = "someone", Password = "wrong" });
-    //
-    //     Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
-    // }
+  
 }
