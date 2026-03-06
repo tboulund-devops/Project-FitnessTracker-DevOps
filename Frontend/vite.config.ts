931fbcfdb31ea61023b9d3e05c717aad53ef9754
MikkelGaming <70634606+MikkelGaming@users.noreply.github.com>
2026-03-06 13:25:45 +0100

    (PATCH) Switched to an open port

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5153',
+        target: 'http://localhost:8050',
         changeOrigin: true,
       },
     },
