caf83396f0baa1e1324740380e56d8935c3e9e96
Jakob2rune <jakjes01@easv365.dk>
2026-04-15 10:27:14 +0200

    (MAJOR) Release: This is the Final Release, and will not be worked on any more. There has been added a new E2E Test and comments in workflow.yml

--- Tests/E2ETest.js
+++ Tests/E2ETest.js
@@ -11,3 +11,11 @@ test('Valid credentials', async t => {
     await t.expect(url).contains('/home');
 });
 
+test('Invalid credentials', async t => {
+    await t.typeText(Selector('#username'), 'invalid')
+        .typeText(Selector('#password'), 'invalid')
+        .click(Selector('button').withText('Log in'));
+    const url = await t.eval(() => window.location.href);
+    await t.expect(url).notContains('/home');
+    await t.expect(Selector('.login-error').innerText).contains('Invalid username or password');
+});
